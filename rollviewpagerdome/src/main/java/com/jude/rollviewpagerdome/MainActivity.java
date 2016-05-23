afe476ca115fd494015c37092dc6d7e83550b27d
maplejaw <i@maplejaw.com>
2016-05-23 13:45:30 +0800

    修改demo

--- rollviewpagerdome/src/main/java/com/jude/rollviewpagerdome/MainActivity.java
+++ rollviewpagerdome/src/main/java/com/jude/rollviewpagerdome/MainActivity.java
@@ -2,42 +2,53 @@ package com.jude.rollviewpagerdome;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 
 import com.jude.rollviewpager.RollPagerView;
 import com.jude.rollviewpager.adapter.LoopPagerAdapter;
-import com.jude.rollviewpager.adapter.StaticPagerAdapter;
 import com.jude.rollviewpager.hintview.IconHintView;
+import com.squareup.picasso.Picasso;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private RollPagerView mRollViewPager;
+    private List<String> mList;
+    private TestLoopAdapter mTestLoopAdapter;
+    private Button mButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mRollViewPager= (RollPagerView) findViewById(R.id.roll_view_pager);
+        mButton= (Button) this.findViewById(R.id.btn);
+        mRollViewPager= (RollPagerView) this.findViewById(R.id.roll_view_pager);
+
+
+
+        mList=new ArrayList<>();
+        mList.add("http://a.hiphotos.baidu.com/zhidao/pic/item/024f78f0f736afc33199a401b119ebc4b7451238.jpg");
+        mList.add("http://dl.bizhi.sogou.com/images/2013/11/26/421428.jpg");
+        mList.add("http://c.hiphotos.baidu.com/zhidao/pic/item/5bafa40f4bfbfbedd9cd7d587bf0f736afc31f04.jpg");
+        mList.add("http://pic51.nipic.com/file/20141016/18941053_114235006000_2.jpg");
+
+
         mRollViewPager.setPlayDelay(1000);
         mRollViewPager.setAnimationDurtion(500);
-        mRollViewPager.setAdapter(new TestLoopAdapter(mRollViewPager));
-//        mRollViewPager.setAdapter(new TestNomalAdapter());
+        mRollViewPager.setAdapter(mTestLoopAdapter=new TestLoopAdapter(mRollViewPager));
+
         mRollViewPager.setHintView(new IconHintView(this,R.drawable.point_focus,R.drawable.point_normal));
-        //mRollViewPager.setHintView(new ColorPointHintView(this, Color.YELLOW,Color.WHITE));
-        //mRollViewPager.setHintView(new TextHintView(this));
-        //mRollViewPager.setHintView(null);
+
+        mButton.setOnClickListener(this);
     }
 
+
+
     private class TestLoopAdapter extends LoopPagerAdapter{
-        private int[] imgs = {
-                R.drawable.img1,
-                R.drawable.img2,
-                R.drawable.img3,
-                R.drawable.img4,
-                R.drawable.img5,
-        };
+
 
         public TestLoopAdapter(RollPagerView viewPager) {
             super(viewPager);
@@ -46,63 +57,30 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public View getView(ViewGroup container, int position) {
             ImageView view = new ImageView(container.getContext());
-            view.setImageResource(imgs[position]);
             view.setScaleType(ImageView.ScaleType.CENTER_CROP);
-            view.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
+            Picasso.with(container.getContext()).load(mList.get(position)).into(view);
             return view;
         }
 
         @Override
         public int getRealCount() {
-            return imgs.length;
+            return mList.size();
         }
 
     }
 
-    private class TestNomalAdapter extends StaticPagerAdapter{
-        private int[] imgs = {
-                R.drawable.img1,
-                R.drawable.img2,
-                R.drawable.img3,
-                R.drawable.img4,
-        };
-
-
-        @Override
-        public View getView(ViewGroup container, int position) {
-            ImageView view = new ImageView(container.getContext());
-            view.setImageResource(imgs[position]);
-            view.setScaleType(ImageView.ScaleType.CENTER_CROP);
-            view.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
-            return view;
-        }
-
-
-        @Override
-        public int getCount() {
-            return imgs.length;
-        }
-    }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+    public void onClick(View v) {
+        mList.clear();
+        mList.add("http://img4.imgtn.bdimg.com/it/u=2920262631,1375942661&fm=21&gp=0.jpg");
+        mTestLoopAdapter.notifyDataSetChanged();
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
 
-        return super.onOptionsItemSelected(item);
-    }
+
+
+
+
 }
